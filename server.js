f99902c8efc55da7fffde0277a403ffdcd4f85af
Mark2848388 <ockasovmark5@gmail.com>
2026-06-26 22:43:12 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const { PeerServer } = require('peer');
-const peerServer = PeerServer({ port: 3000, path: '/myapp' });
-peerServer.on('connection', (client) => {
-  console.log('Клиент подключился:', client.id);
+const peerServer = PeerServer({
+    port: 9000,
+    path: '/myapp' // Этот путь важен!
 });
-console.log('PeerJS сервер запущен на порту 3000');
+console.log('🚀 PeerJS сервер запущен');
+```[citation:12]
+Именно этот путь (`/myapp`) вы указываете в своём HTML-коде. Если он отличается, соединения не будет [citation:12].
