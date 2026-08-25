408ecefcb39469093eccfbd826c30299f8b85986
Mark2848388 <ockasovmark5@gmail.com>
2026-08-25 12:15:20 +0500

    Update server.js

--- server.js
+++ server.js
@@ -7,30 +7,20 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Раздаем статические файлы (HTML, CSS, JS) где бы они ни лежали
+// Раздаем статические файлы (CSS, JS) прямо из корня
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'public')));
 
-// Главная страница (если файл лежит в public, берем его оттуда)
+// Главная страница: ВСЕГДА отдаем index.html из корня
 app.get('/', (req, res) => {
-    const indexPath = path.join(__dirname, 'public', 'index.html');
-    res.sendFile(indexPath, (err) => {
-        if (err) {
-            // Если в public нет, пробуем найти в корне
-            res.sendFile(path.join(__dirname, 'index.html'));
-        }
-    });
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Сигнальный сервер для WebRTC
 io.on('connection', (socket) => {
     console.log('Пользователь подключился:', socket.id);
-    
     socket.on('join-room', (roomId) => {
         socket.join(roomId);
         socket.to(roomId).emit('user-connected', socket.id);
     });
-    
     socket.on('offer', (offer, roomId) => socket.to(roomId).emit('offer', offer));
     socket.on('answer', (answer, roomId) => socket.to(roomId).emit('answer', answer));
     socket.on('ice-candidate', (candidate, roomId) => socket.to(roomId).emit('ice-candidate', candidate));
